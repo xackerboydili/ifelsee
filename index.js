535250b18edf25a238643d66b9400f3edfe1bc70
xackerboydili <isoqovdilshod47@gmail.com>
2022-10-31 18:45:42 +0500

    Strings

---
+++ index.js
@@ -0,0 +1,152 @@
+// "1-chi masala"
+// function nameString(name){
+// 	let a = "Edabit"
+//     let result = `${a}${name}`
+//     return result
+// }
+// console.log(nameString("Mubashir"))
+
+// function nameString(name){
+// 	let a = "Edabit"
+//     let result = a.concat(name)
+//     return result
+// }
+// console.log(nameString("Mubashir"))
+
+// "2-chi masala"
+
+// function boolToString(flag) {
+// 	return flag == true ? "true" : "false"
+// }
+// console.log(boolToString(false))
+
+// "3-chi masala"
+
+// function comp(str1, str2) {
+// 	return str1.length == str2.length ? true : false
+// }
+// console.log(comp("abc", "ekds"))
+
+// "4-chi masala"
+
+// function isEmpty(s) {
+// 	return s == "" ? true : false
+// }
+// console.log(isEmpty("a"))
+
+// "5-chi masala"
+
+// function length(str) {
+// 	let length = str.length
+//     return length
+// }
+// console.log(length(" "))
+
+// "6-chi masala"
+
+// function stringInt(str) {
+// 	return Number(str)
+// }
+// console.log(stringInt("32"))
+
+// "7-chi masala"
+
+// function concatName(firstName, lastName) {
+//     let a = firstName
+// 	return lastName.concat(a)
+// }
+// console.log(concatName("John", "doe "))
+
+// function concatName(firstName, lastName) {
+// 	let a = firstName
+//     return `${lastName} ${a}`
+// }
+// console.log(concatName("John", "Doe"))
+
+// "8-chi masala"
+
+// function format(a, b, c) {
+//     let name1 = a
+//     let name2 = b
+//     let name3 = c
+//     return `${name1}'s friends are ${name2} and ${name3}`
+// }
+// console.log(format("Dilshod", "Sarvar", "Mashon"))
+
+// "9-chi masala"
+
+// function intToString(num) {
+// 	return String(num)
+// }
+// console.log(intToString(5))
+
+// function stringToInt(str) {
+// 	return Number(str)
+// }
+// console.log(stringToInt("5"))
+// "10-chi masala"
+
+// function newWord(str) {
+// 	let result = str.substring(1)
+//     return result
+// }
+
+// console.log(newWord("Dilshod"))
+
+// "11-chi masala"
+
+
+
+// function helloName(name){
+//     return `Hello ${name}!`
+// }
+// console.log(helloName("Dilshod"))
+
+// "12-chi masala"
+// function relationToLuke(name) {
+// 	return name === "Darth Vader" ? "Luke, I am your father" :
+//            name === "Leia" ? "Luke I am your sister": "I am noone for you"
+
+// }
+// console.log(relationToLuke("Dilshod"))
+
+// "13-masala"
+// let city={
+//     name : "Uzbekistan",
+//     population : "over 35 million",
+//     continent : "Asia",
+
+// }
+// function cityFacts(city) {
+    
+// 	let result = `${city.name} has ${city.population} and located in ${city.continent}`;
+//     return result}
+// console.log(cityFacts({
+//     name : "Uzbekistan",
+//     population : "over 35 million",
+//     continent : "Asia",
+
+// }))
+
+// "14-chi masala"
+// function isPlural(word) {
+//     return word.endsWith("s") ? true :false
+// }
+// console.log(isPlural("comp"))
+
+// "15-chi masala"
+
+// function repeatString(txt, n) {
+// 	return txt.repeat(n)
+// }
+// console.log(repeatString("Alo", 4))
+
+
+
+// let a = "Jasavscript";
+// let nimadir = a.startsWith('Ja')  
+// console.log(nimadir)
+
+
+
+
